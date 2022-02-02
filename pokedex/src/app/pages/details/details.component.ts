1bdff66f4bbbe9dbfcacdfc41a790f25ea1bbfdd
lucasilmor <77138273+lucasilmor@users.noreply.github.com>
2022-02-02 19:18:32 -0300

    configuração inicial do details component

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private PokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.pokemon;
   }
 
+  get pokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.PokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.PokeApiService.apiGetPokemons(`${this.urlName}/${id}`)
+
+    return forkJoin([pokemon, name]).subscribe(
+      res =>{
+        console.log(res);
+      }
+    );
+  }
 }
