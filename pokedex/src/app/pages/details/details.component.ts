cfd208970b8ca90f92accd09b33f57d66978ffeb
lucasilmor <77138273+lucasilmor@users.noreply.github.com>
2022-02-02 20:24:43 -0300

    mensagem de erro e correção de bugs

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -14,7 +14,8 @@ export class DetailsComponent implements OnInit {
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
   public pokemon: any;
-
+  public isLoading: boolean = false;
+  public apiError: boolean = false
 
   constructor(
     private activatedRoute: ActivatedRoute, 
@@ -33,6 +34,10 @@ export class DetailsComponent implements OnInit {
     return forkJoin([pokemon, name]).subscribe(
       res =>{
           this.pokemon = res;
+          this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
       }
     );
   }
